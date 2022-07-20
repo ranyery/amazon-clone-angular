8761982fc2548367bc4c3db468ec925634ccf8e9
ranyery <ranyerysc@gmail.com>
2022-07-20 19:24:43 -0300

    feat: add hero component

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero.component.scss'],
 })
 export class HeroComponent implements OnInit {
+  public indexBG: number = 1;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  public previousBackground() {
+    this.indexBG = this.indexBG === 1 ? 3 : this.indexBG - 1;
+  }
+
+  public nextBackground() {
+    this.indexBG = this.indexBG === 3 ? 1 : this.indexBG + 1;
+  }
 }
